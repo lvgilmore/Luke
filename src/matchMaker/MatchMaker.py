352b143c2918a7d0b2705b527747e4ff23c6b708
yuliaTev <ytev91@gmail.com>
2016-09-19 19:48:06 +0300

    config file added

--- src/matchMaker/MatchMaker.py
+++ src/matchMaker/MatchMaker.py
@@ -1,15 +1,19 @@
+from ConfigParser import SafeConfigParser, NoSectionError, ConfigParser, NoOptionError
 from logging import getLogger
 
+from src.utils.ConfFileUtil import ConfFileUtil
+
 logger = getLogger(__name__)
+SECTION = 'asection'
 
 
 class MatchMaker:
 
     def __init__(self):
-        pass
+        # read scores from file
+        self.parser = ConfFileUtil.read_from_conf_file('../resources/scores.conf')
 
-    @staticmethod
-    def find_match_by_all_values(bare_metal, req_list):
+    def find_match_by_all_values(self, bare_metal, req_list):
 
         """
         finds if there are equal keys and values in bare metal and in one of a requests
@@ -24,19 +28,19 @@ class MatchMaker:
         for request in req_list:
             curr_req_score = 0
             for bare_metal_key in bare_metal.keys():
-                if request.requirements:
-                    if bare_metal_key in request.requirements:
-                        if bare_metal[bare_metal_key] == request.requirements[bare_metal_key]:
-                            curr_req_score += MatchMaker.calc_score(bare_metal_key)
+                if request.requirements is not None and\
+                                bare_metal_key in request.requirements and\
+                                bare_metal[bare_metal_key] == request.requirements[bare_metal_key]:
+                    curr_req_score += self.calc_score(bare_metal_key)
 
-                elif request.other_prop:
-                    if bare_metal_key in request.other_prop:
-                        if bare_metal[bare_metal_key] == request.other_prop[bare_metal_key]:
-                            curr_req_score += MatchMaker.calc_score(bare_metal_key)
+                elif request.other_prop is not None and\
+                    bare_metal_key in request.other_prop and\
+                    bare_metal[bare_metal_key] == request.other_prop[bare_metal_key]:
+                        curr_req_score += self.calc_score(bare_metal_key)
 
-                elif bare_metal_key in request.os:
-                    if bare_metal[bare_metal_key] == request[bare_metal_key]:
-                        curr_req_score += MatchMaker.calc_score(bare_metal_key)
+                elif bare_metal_key in request.os and\
+                                bare_metal[bare_metal_key] == request[bare_metal_key]:
+                    curr_req_score += self.calc_score(bare_metal_key)
 
             # compare by score
             if curr_req_score > best_match_req['score']:
@@ -59,9 +63,8 @@ class MatchMaker:
 
         for request in req_list:
             request_match = True
-            if request.requirements:
+            if request.requirements is not None:
                 for requirements_key in request.requirements:
-                    # if request_match:
                     if requirements_key not in bare_metal.keys():
                         request_match = False
                         break
@@ -74,19 +77,23 @@ class MatchMaker:
 
         return matched_req_by_requirements
 
-    @staticmethod
-    def calc_score(key):
+    # TODO decide what we prefer: there is another option, to use sections, and declare a default section,
+    #  so when the score is not declared, it will search it in defaule section and init score with 0
+    def calc_score(self, key):
         score = 0
 
-        # TODO read scores from a config file
-
-        if key == "name":
-            score = 2
-        elif key == "url":
-            score = 4
+        try:
+            score = int(self.parser.get(SECTION, key))
+        except NoSectionError as nse:
+            print "calc_score: " + "NoSectionError " + nse.message
+        except NoOptionError as noe:
+            print "calc_score: " + "NoOptionError" + noe.message
+        except ValueError as ve:
+            print "calc_score: " + "ValueError " + ve.message
 
         return score
 
 
 if __name__ == "__main__":
-    pass
+    mm = MatchMaker()
+
