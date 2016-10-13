47ecad99115389fcf195fd4197f43e466dbfa8e0
lvgilmore <geigereitan@gmail.com>
2016-10-13 12:06:54 +0300

    BUGFIX: broken django dependencies - django used to fail over Luke.* imports - turns out when it sees netsted directories with the same name,    it changes the path to the bottom one - changed django directory name seems to solve everything

--- Luke/API/settings.py
+++ Luke/API/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Luke',
-    'LukeRest',
+    'Luke.API',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'LukeRest.urls'
+ROOT_URLCONF = 'API.urls'
 
 TEMPLATES = [
     {
